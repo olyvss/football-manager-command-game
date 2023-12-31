806b39716d7864bc19aa8210f2e33838e2991f15
joseph <135014672+olyvss@users.noreply.github.com>
2023-12-31 16:12:57 +1300

    Update game.cs
    not allowed to generate another team

--- game.cs
+++ game.cs
@@ -12,25 +12,32 @@ namespace PremierLeagueTeamGenerator
                 "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest",
                 "Sheffield United", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"
             };
-
+            
+        
+            string commandTeam = "Your randomly generated Premier League team is: {0}";
+          
+            int randomIndex;
+            
             Random random = new Random();
 
             while (true)
-            {
+            {;
                 Console.Write("Enter a command (type 'help' for options): ");
                 string command = Console.ReadLine().ToLower();
 
-                if (command == "team")
+                if (command.ToLower() == "team")
                 {
-                    int randomIndex = random.Next(teams.Length);
+                    randomIndex = random.Next(teams.Length);
                     string randomTeam = teams[randomIndex];
-                    Console.WriteLine("Your randomly generated Premier League team is: {0}", randomTeam);
+                    Console.WriteLine(commandTeam, randomTeam);
+                    commandTeam = "you cant generate another team" + System.Environment.NewLine;
                 }
                 else if (command == "help")
                 {
                     Console.WriteLine("Available commands:");
                     Console.WriteLine("- team: Generates a random Premier League team.");
                     Console.WriteLine("- quit: Exits the game.");
+                    
                 }
                 else if (command == "quit")
                 {
